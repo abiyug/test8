e25eff90d979d8b03f320b73d27c55e9f5315e84
Abiyu Giday <abiyu.giday@antexusolutions.com>
2020-10-19 14:52:56 -0700

    First Commit.

---
+++ R/add_two.R
@@ -0,0 +1,15 @@
+#' @title Add plus 2 to a number
+#'
+#' @description This functoin adds 2 to any number provided.
+#'
+#' @param x A number (integer)
+#' @return a number object
+#' @export
+#'
+#' @examples
+#' add_two(2)
+#' add_two(98)
+#' add_two(-8)
+add_two <- function(x) {
+  x + 2
+}
---
+++ R/df_1.R
@@ -0,0 +1,12 @@
+#' @title truncated_mtcars
+#' @description The data was extracted from the 1974 Motor Trend US magazine
+#' @format A data frame with 5 rows and 5 variables:
+#' \describe{
+#'   \item{\code{mpg}}{double Miles/(US) gallon}
+#'   \item{\code{cyl}}{double Number of cylindersN}
+#'   \item{\code{disp}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{hp}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{drat}}{double COLUMN_DESCRIPTION}
+#'}
+#' @source \url{http://www.datarecode.com/}
+"df_1"
